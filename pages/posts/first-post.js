a6f8cd171bec68f4515dda327f665bd67d23d383
liu <shellingfordly@qq.com>
2021-11-03 17:12:43 +0800

    feat: add first-post page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,16 @@
 import Link from "next/link";
 
+import Image from "next/image";
+
+const YourComponent = () => (
+  <Image
+    src="/images/react.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+);
+
 export default function FirstPost() {
   return (
     <>
@@ -9,6 +20,7 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
+      <YourComponent />
     </>
   );
 }
